9c14c0a20b797c9d3b8b7db64a8d0266f08cf3fc
Rajeev Mukundan <rmukunda@andrew.ad.cmu.edu>
2016-10-22 16:48:55 -0400

    PassCode Panel Added

---
+++ Assets/Scripts/PanelButton.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public class PanelButton : MonoBehaviour
+{
+
+    public UnityEvent onButtonPress;
+
+    private Transform buttonVisual;
+    private Renderer buttonRenderer;
+
+    // Use this for initialization
+    void Start()
+    {
+        buttonVisual = transform.Find("Visual");
+        buttonRenderer = buttonVisual.GetComponent<Renderer>();
+    }
+
+    void FixedUpdate()
+    {
+        buttonVisual.localPosition = Vector3.Lerp(buttonVisual.localPosition, Vector3.zero, 0.2f);
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter()
+    {
+        onButtonPress.Invoke();
+        buttonVisual.localPosition = Vector3.back * 0.0009f;
+    }
+}
---
+++ Assets/Scripts/PassCode.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PassCode : MonoBehaviour {
+
+    private float lastTimeOfEnterCode;
+    public TextMesh passCodeDisplay;
+
+    private bool waitForCodeCheck = false;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+
+    public void EnterCode(int codeValue)
+    {
+
+        if (waitForCodeCheck == false)
+        {
+            if (Time.time - lastTimeOfEnterCode < 0.5f) // Can only press button every 0.75 seconds
+            {
+                return;
+            }
+
+            lastTimeOfEnterCode = Time.time;
+
+            if (passCodeDisplay.text == "----")
+            {
+                passCodeDisplay.text = codeValue.ToString();
+            }
+            else
+            {
+                passCodeDisplay.text += codeValue;
+            }
+
+            if (passCodeDisplay.text.Length == 4)
+            {
+                waitForCodeCheck = true;
+                StartCoroutine(checkCode(int.Parse(passCodeDisplay.text)));
+            }
+        }
+    }
+
+    public IEnumerator checkCode(int code)
+    {
+        yield return new WaitForSeconds(0.75f);
+        passCodeDisplay.text = "RETRY";
+        yield return new WaitForSeconds(1f);
+        passCodeDisplay.text = "----";
+        waitForCodeCheck = false;
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
