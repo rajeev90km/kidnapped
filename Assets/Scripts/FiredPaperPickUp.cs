8d24f89c4d2101b275e5bd99dc67abddf246a5f5
Yiwen Zhang <tiotaocn@gmail.com>
2016-10-22 20:18:15 -0400

    add couple arguing

---
+++ Assets/Scripts/FiredPaperPickUp.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FiredPaperPickUp : MonoBehaviour {
+
+    bool isTrackPlayed = false;
+    public GameObject argueObject;
+    AudioSource argueAudioSource;
+
+	// Use this for initialization
+	void Start () {
+        argueAudioSource = argueObject.GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	    if (transform.parent && !isTrackPlayed)
+        {
+            argueAudioSource.Play();
+            isTrackPlayed = true;
+        }
+	}
+
+    
+}
