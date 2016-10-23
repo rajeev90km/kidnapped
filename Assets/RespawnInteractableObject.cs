6c1b7f65f199ff7bdca4d3136c484ec2b76c0a72
Yiwen Zhang <tiotaocn@gmail.com>
2016-10-23 15:38:23 -0400

    respawn objects when it get threw out of the room, using ontriggerexit

---
+++ Assets/RespawnInteractableObject.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RespawnInteractableObject : MonoBehaviour {
+
+    public Vector3 _defaultPosition = new Vector3(-1f, 1f, -0.5f);
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+	}
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag(Tags.Interactable))
+        {
+            other.gameObject.transform.position = _defaultPosition;
+        }
+    }
+}
