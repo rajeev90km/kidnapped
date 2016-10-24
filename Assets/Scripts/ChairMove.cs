9ed9ca6d9679d990e2e00d28489e93a3150cb541
team62 <duduyi2015@gmail.com>
2016-10-23 23:07:55 -0400

    Simple Chair move script

---
+++ Assets/Scripts/ChairMove.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChairMove : MonoBehaviour {
+
+    GameObject cameraEye;
+
+    float currentTime;
+	// Use this for initialization
+	void Start () {
+        cameraEye = GameObject.Find("Camera (eye)");
+
+        InvokeRepeating("UpdateChairPosition", 1.0f, 1.0f);
+    }
+	
+    void UpdateChairPosition()
+    {
+        if (cameraEye) { 
+            if (cameraEye.transform.position.y > 1.0f)
+               transform.position = new Vector3(cameraEye.transform.position.x, transform.position.y, cameraEye.transform.position.z);
+        }
+
+    }
+    // Update is called once per frame
+    void Update () {
+        
+	}
+}
