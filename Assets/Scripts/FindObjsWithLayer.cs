61896e9336fa13f7bd30debff1b1290ef0220909
JoeETC <duduyi2015@gmail.com>
2016-10-24 15:40:52 -0400

    new script for finding unwanted object with specific layer

--- Assets/Scripts/FindObjsWithLayer.cs
+++ Assets/Scripts/FindObjsWithLayer.cs
@@ -1,15 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class FindObjsWithLayer : MonoBehaviour {
+    [SerializeField]
+    List<GameObject> target;
+    int _targetLayer;
 
+    List<GameObject> _unwantedObjs;
 	// Use this for initialization
 	void Start () {
-	
+        _unwantedObjs = new List<GameObject>();
+        _targetLayer = LayerMask.NameToLayer(Layers.Sharp);
+        RecursiveFindTransform(transform, _targetLayer, target);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    void  RecursiveFindTransform(Transform parent, int _tarLayer, List<GameObject> target) {
+        foreach(Transform child in parent) {
+            RecursiveFindTransform(child, _tarLayer, target);
+        }
+        if (parent.gameObject.layer == _tarLayer) {
+            if (!target.Contains(parent.gameObject)) {
+                _unwantedObjs.Add(parent.gameObject);
+                Debug.Log("Unwanted: " + parent.name);
+            }
+        }
+    }
 }
--- Assets/Scripts/Layers.cs
+++ Assets/Scripts/Layers.cs
@@ -4,4 +4,5 @@ using System.Collections;
 public static class Layers{
     public const string Sharp = "Sharp";
     public const string FingerTip = "FingerTip";
+    public const string PassCode = "PassCode";
 }
