1eb2081ddcbb9c702e9baf4051adeeabf8b0da4c
Yiwen Zhang <tiotaocn@gmail.com>
2016-10-24 13:06:16 -0400

    add radio light

---
+++ Assets/RadioLight.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RadioLight : MonoBehaviour {
+
+    private Light _light;
+    public float _maxIntensity;
+
+	// Use this for initialization
+	void Start () {
+        _light = GetComponent<Light>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        _light.intensity = Mathf.PingPong(Time.time*_maxIntensity, _maxIntensity);
+    }
+}
