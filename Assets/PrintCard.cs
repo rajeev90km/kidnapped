f23819da736b6cc09d216049dc10fdde9ddec9d4
Yiwen Zhang <tiotaocn@gmail.com>
2016-10-24 13:51:36 -0400

    update radio texture and card

--- Assets/PrintCard.cs
+++ Assets/PrintCard.cs
@@ -4,11 +4,15 @@ using System.Collections;
 public class PrintCard : MonoBehaviour {
 
     public bool _printCard;
+    private bool _playAudio;
+    private AudioSource _audio;
     private Vector3 _destination;
 
 	// Use this for initialization
 	void Start () {
         _printCard = false;
+        _playAudio = false;
+        _audio = GetComponent<AudioSource>();
         _destination = new Vector3(0.0149f, 0.9746f, -2.2173f);
     }
 	
@@ -16,6 +20,11 @@ public class PrintCard : MonoBehaviour {
 	void Update () {
 	    if (_printCard)
         {
+            if (!_playAudio && !_audio.isPlaying)
+            {
+                _audio.Play();
+                _playAudio = true;
+            }
             transform.position = Vector3.MoveTowards(transform.position, _destination, Time.deltaTime * 0.1f);
         }
 	}
