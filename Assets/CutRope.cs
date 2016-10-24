df40abbe42eb29269bad4536df8f7ca9ce847005
Yiwen Zhang <tiotaocn@gmail.com>
2016-10-23 23:56:11 -0400

    update some amazing solution for chairs

---
+++ Assets/CutRope.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CutRope : MonoBehaviour {
+
+    public GameObject _chair;
+    private AudioSource _audio;
+
+	// Use this for initialization
+	void Start () {
+        _audio = GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "brokenBottleTop")
+        {
+            GetComponent<Animator>().SetBool("RopeCut", true);
+            StartCoroutine(SetFree());
+        }
+    }
+
+    IEnumerator SetFree()
+    {
+        yield return new WaitForSeconds(2f);
+        _audio.Play();
+        _chair.SetActive(false);
+        gameObject.GetComponent<Rigidbody>().useGravity = true;
+    }
+}
