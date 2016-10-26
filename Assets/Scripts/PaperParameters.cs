740cb2f5312f70f87862e1481efe14c41a759a2e
JoeETC <duduyi2015@gmail.com>
2016-10-26 18:31:45 -0400

    two scenes in the same scene and add stamping mechanics into the game

---
+++ Assets/Scripts/PaperParameters.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaperParameters : MonoBehaviour {
+
+
+    bool _isStampOnMe;
+
+    void Start() {
+        _isStampOnMe = false;
+    }
+
+    public void StampON() {
+        _isStampOnMe = true;
+    }
+
+    public bool IsStampOnMe() {
+        return _isStampOnMe;
+    }
+}
---
+++ Assets/Scripts/Stamp.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Stamp : MonoBehaviour {
+    bool _point1Dected;
+    bool _point2Dected;
+    bool _point3Dected;
+
+
+
+    void Start () {
+
+        _point1Dected = false;
+        _point2Dected = false;
+        _point3Dected = false;
+        int i = 0;
+        foreach(Transform child in transform) {
+            child.gameObject.GetComponent<StampDetect>().SetMyIndex(i);
+            i++;
+        }
+    }
+
+
+    public bool _isAllPointsDetected() {
+        if (_point1Dected && _point2Dected && _point3Dected) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void SetPointDetectFlag(int indexOfChild, bool tof) {
+        switch (indexOfChild) {
+            case 0:
+                _point1Dected = tof;
+                break;
+            case 1:
+                _point2Dected = tof;
+                break;
+            case 2:
+                _point3Dected = tof;
+                break;
+            default:
+                Debug.Log("Error index of stamp child");
+                break;
+        }
+    }
+}
