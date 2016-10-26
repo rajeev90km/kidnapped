3b7cca62f67f7a104bd351c1dc1796cb2c05366a
Yiwen Zhang <tiotaocn@gmail.com>
2016-10-26 18:17:54 -0400

    UPDATE FAKE HAND TRACKING

--- Assets/Scripts/FakeHand.cs
+++ Assets/Scripts/FakeHand.cs
@@ -2,9 +2,15 @@
 using System.Collections;
 
 public class FakeHand : MonoBehaviour {
+
     [SerializeField]
     Transform _realHand;
+    Vector3 _realHandPrevPos;
     bool _isChairDestroyed;
+
+    public float TRACKING_THRESHOLD = 0.004f;
+    
+
     // Use this for initialization
     void Start () {
         _isChairDestroyed = false;
@@ -13,9 +19,26 @@ public class FakeHand : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        transform.position = _realHand.position;
-        transform.rotation = Quaternion.Euler(0, _realHand.rotation.eulerAngles.y, 0);
-	}
+        if (HandMoved())
+        {
+            transform.position = _realHand.position;
+            transform.rotation = Quaternion.Euler(0, _realHand.rotation.eulerAngles.y, 0);
+            _realHandPrevPos = _realHand.position;
+        }
+        
+        
+    }
+
+    private bool HandMoved()
+    {
+        if (Vector3.Distance(_realHand.position, _realHandPrevPos) > TRACKING_THRESHOLD)
+        {
+            return true;
+        } else {
+            return false;
+        };
+        
+    }
 
     public void SetChairDestroyedFlag() {
         _isChairDestroyed = true;
