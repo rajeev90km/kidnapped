b26d3d809f9356b5a856d04a904dbd72bc1a167c
Rajeev Mukundan <rmukunda@andrew.ad.cmu.edu>
2016-10-26 20:26:39 -0400

    Persist Facebook User info multiple times

--- Assets/Scripts/OAuthTemplates/UserObj.cs
+++ Assets/Scripts/OAuthTemplates/UserObj.cs
@@ -5,8 +5,8 @@ public class UserObj
 
     public string first_name;
     public string last_name;
-    public string gender;
     public string id;
+    public string picture_url;
 
     public static UserObj CreateFromJson(string jsonStr)
     {
