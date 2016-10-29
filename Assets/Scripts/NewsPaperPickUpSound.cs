ff1f9a0f7264e6fcaa19995d7231db3ae7037d61
JoeETC <duduyi2015@gmail.com>
2016-10-28 21:39:36 -0400

    add relative materials

---
+++ Assets/Scripts/NewsPaperPickUpSound.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewsPaperPickUpSound : MonoBehaviour {
+    public TVSoundEffect _tvSound;
+
+    void OnCollisionEnter(Collision colli) {
+        if (colli.gameObject.layer == LayerMask.NameToLayer(Layers.Palm)) {
+            _tvSound.PlayNews();
+        }
+    }
+
+}
---
+++ Assets/Scripts/TVSoundEffect.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TVSoundEffect : MonoBehaviour {
+
+    public AudioClip _TVBGM;
+    public AudioClip _TVNews;
+
+    AudioSource _TVBGMSource;
+    public AudioSource _TVNewsSource;
+
+    public float _fadeOutFactor = 1f;
+    bool _isFadeInOut;
+    float _silenceThreshold = 0.01f;
+	// Use this for initialization
+	void Start () {
+        _isFadeInOut = false;
+        _TVBGMSource = GetComponent<AudioSource>();
+        PlayBGM();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (_isFadeInOut) {
+            if (_TVBGMSource.volume > 0) {
+                _TVBGMSource.volume /= _fadeOutFactor;
+                if (_TVBGMSource.volume < _silenceThreshold) {
+                    _TVBGMSource.volume = 0.0f;
+                }
+            }
+        }
+	}
+
+    public void PlayBGM() {
+        _TVBGMSource.clip = _TVBGM;
+        _TVBGMSource.Play();
+    }
+
+    public void PlayNews() {
+        if (_TVBGMSource.isPlaying) {
+            _isFadeInOut = true;
+            _TVNewsSource.clip = _TVNews;
+            _TVNewsSource.Play();
+        }
+    }
+}
