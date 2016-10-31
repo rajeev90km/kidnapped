7e20635f3d0c860980c83e829385c61cec7d6bc5
Yiwen Zhang <tiotaocn@gmail.com>
2016-10-31 16:43:39 -0400

    update lucy slow

--- Assets/Scripts/Blink.cs
+++ Assets/Scripts/Blink.cs
@@ -6,6 +6,7 @@ public class Blink : MonoBehaviour {
 
     public int direction;
     private int ctr = 0;
+    private AudioSource audio;
 
     float startTime;
 
@@ -16,6 +17,7 @@ public class Blink : MonoBehaviour {
     void Awake()
     {
         startTime = Time.timeSinceLevelLoad;
+        audio = GetComponent<AudioSource>();
         
     }
 
@@ -27,6 +29,7 @@ public class Blink : MonoBehaviour {
         transform.localEulerAngles = new Vector3(0,0,0);
         //cameraBlur = cameraObj.GetComponent<Blur>();
         //cameraBlur.enabled = true;
+        StartCoroutine(wakeUpSound());
     }
 	
 	// Update is called once per frame
@@ -35,11 +38,22 @@ public class Blink : MonoBehaviour {
         StartCoroutine(BlinkEye());
     }
 
+    IEnumerator wakeUpSound()
+    {
+        yield return new WaitForSeconds(8f);
+        if (!audio.isPlaying)
+        {
+            audio.Play();
+        }
+    }
+
     IEnumerator BlinkEye()
     {
         if (!stopBlinking)
         {
             yield return new WaitForSeconds(7f);
+            
+            
             ctr++;
             transform.localPosition = new Vector3(-0.11f, transform.localPosition.y + (direction * Mathf.Sin(Time.time - startTime) /10000 * ctr), 0.06f);
             yield return new WaitForSeconds(3f);
